5c645c025b2feab1e1c319dc5a486bde3170debf
evaraicu <evamaria.2003@yahoo.com>
2026-01-14 21:33:08 +0200

    Add files via upload

---
+++ MovieRecommenderGrpcService/Models/MovieRatingData.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+namespace MovieRecommenderGrpcService.Models
+{
+    public class MovieRatingData
+    {
+        [LoadColumn(0)]
+        public float userId;
+        [LoadColumn(1)]
+        public float movieId;
+        [LoadColumn(2)]
+        public float Label;
+    }
+}
